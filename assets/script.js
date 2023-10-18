33373e99c3c42e34ea8d0723d2eb70759d638d3e
Manendra Agnihotri <agnihotri2810@gmail.com>
2023-10-18 11:48:47 +0530

    update

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const activeButton = document.querySelector(".header_main-ham-menu");
+const closeButton = document.querySelector(".header_main-ham-menu-cross");
+const navItem = document.querySelector(".header_sm-menu");
+
+activeButton.addEventListener("click", ()=>{
+    activeButton.classList.add("close-pannel");
+    activeButton.classList.remove("active-pannel");
+    closeButton.classList.add("active-pannel");
+    closeButton.classList.remove("close-pannel");
+
+    navItem.classList.add("active-pannel");
+    navItem.classList.remove("close-pannel");
+
+});
+
+closeButton.addEventListener("click", ()=>{
+    closeButton.classList.add("close-pannel");
+    closeButton.classList.remove("active-pannel");
+    activeButton.classList.add("active-pannel");
+    activeButton.classList.remove("close-pannel");
+
+    navItem.classList.add("close-pannel");
+    navItem.classList.remove("active-pannel");
+});
